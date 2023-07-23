efb6ecdd0ee3de36202c1f392a88eb92e20da7ba
Peter LeCavalier <peterlecavalier@gmail.com>
2023-07-23 09:10:19 -0600

    Buggy site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,6 +54,11 @@
         cursor: pointer;
       }
     }
+
+    .view {
+      height: 400px;
+      width: 800px;
+    }
       
     #a { grid-area: "a"; }
 
@@ -68,3 +73,20 @@
     #f { grid-area: "f"; }
 
   </style>
+
+
+<script>
+  import MapView from "@arcgis/core/views/MapView";
+  import "@arcgis/core/assets/esri/themes/light/main.css";
+
+  const createMap = (domNode) => {
+    const view = new MapView({
+      container: domNode,
+      map: {
+        basemap: "streets-vector",
+      },
+      zoom: 15,
+      center: [-90.1928, 38.6226], // longitude, latitude
+    });
+  };
+</script>
