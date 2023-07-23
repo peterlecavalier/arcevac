c891824430e2b715255ff26f57308f1ccdc04afb
Peter LeCavalier <peterlecavalier@gmail.com>
2023-07-23 11:41:54 -0600

    functioning indoor map!

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,20 +1,67 @@
 <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
 <script>
-  // ADD these 2 lines:
   import MapView from "@arcgis/core/views/MapView";
+  import esriConfig from "@arcgis/core/config";
+  import ScaleBar from "@arcgis/core/widgets/Scalebar";
+  import Legend from "@arcgis/core/widgets/Legend";
+  import FeatureLayer from "@arcgis/core/layers/FeatureLayer"
   import "@arcgis/core/assets/esri/themes/light/main.css";
 
-  // ADD this function:
+  esriConfig.apiKey = "AAPK80848b2e603341559f0b825f203064c4jjuOzGRGwi6Nt2iDESThlpvLccJ_t8DeEUrNEh60gEDc8Oz7TcxE6vCBVIAB5st6";
+
+  const baseUrl = "https://services8.arcgis.com/LLNIdHmmdjO2qQ5q/arcgis/rest/services/Denver_First_Floor_1110/FeatureServer";
+
   const createMap = (domNode) => {
     const view = new MapView({
       container: domNode,
       map: {
-        basemap: "streets-vector",
+        basemap: "streets-night-vector",
       },
-      zoom: 15,
-      center: [-90.1928, 38.6226], // longitude, latitude
+      zoom: 19,
+      center: [-105.12133, 39.97018]
+      // zoomed center: [-105.12132251901546, 39.97027048328751]
     });
+
+    const evacueeLayer = new FeatureLayer({
+      url: baseUrl + "/0"
+    });
+    const exitsLayer = new FeatureLayer({
+      url: baseUrl + "/1"
+    });
+    const initialRouteLayer = new FeatureLayer({
+      url: baseUrl + "/3"
+    });
+    const initialRoutePlusLayer = new FeatureLayer({
+      url: baseUrl + "/5"
+    });
+    const editedRouteLayer = new FeatureLayer({
+      url: baseUrl + "/2"
+    });
+    const editedRoutePlusLayer = new FeatureLayer({
+      url: baseUrl + "/4"
+    });
+    const pathwaysLayer = new FeatureLayer({
+      url: baseUrl + "/6"
+    });
+    const hallwayBlockedLayer = new FeatureLayer({
+      url: baseUrl + "/7"
+    });
+    const geofenceLayer = new FeatureLayer({
+      url: baseUrl + "/8"
+    });
+    const roomsLayer = new FeatureLayer({
+      url: baseUrl + "/9"
+    });
+
+    view.map.add(geofenceLayer, 0);
+    view.map.add(pathwaysLayer, 0);
+    view.map.add(exitsLayer);
+    view.map.add(evacueeLayer);
+    view.map.add(initialRouteLayer);
+    view.map.add(initialRoutePlusLayer);
   };
+
+  
 </script>
 
 <main>
@@ -25,6 +72,10 @@
 
   <a href="/stuck">
     <button class="btn-stuck"> I am stuck</button>
+  </a>
+
+  <input id="next" type="button" value=">" onclick="">
+    <button class="btn-next"> </button>
   </a>
   
   
